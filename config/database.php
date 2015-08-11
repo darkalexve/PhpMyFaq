42e4a8285d8c79a68bd640c58b5099cd58bf81dd
Jonathan Melendez <darkalexve@gmail.com>
2015-08-11 15:39:08 -0430

    Editando Data Base

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-$DB['server'] = 'sql206.byetcluster.com';
-$DB['user'] = '16217430_4';
-$DB['password'] = '[S9p5P1[fs';
-$DB['db'] = 'b12_16217430_kvm';
+$DB['server'] = 'localhost';
+$DB['user'] = 'root';
+$DB['password'] = 'getsuga95';
+$DB['db'] = 'kbsystem';
 $DB['prefix'] = 'kvp2_';
 $DB['type'] = 'mysqli';
