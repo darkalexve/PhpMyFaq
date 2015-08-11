1731df70b1c3a7d394ae48d4241f3245ed1e19f7
Jonathan Melendez <jmelendez@contactop2p.com>
2015-08-11 15:34:03 -0430

    Añadiendo Todo el Proyecto

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+$DB['server'] = 'sql206.byetcluster.com';
+$DB['user'] = '16217430_4';
+$DB['password'] = '[S9p5P1[fs';
+$DB['db'] = 'b12_16217430_kvm';
+$DB['prefix'] = 'kvp2_';
+$DB['type'] = 'mysqli';
